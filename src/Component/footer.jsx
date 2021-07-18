8b2ca92f27b22cd8cfd20a1df6daa757db78d4f7
anandvyas1290 <anandvyas1290@gmail.com>
2021-07-18 16:43:41 +0530

    adding files

---
+++ src/Component/footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+// Footer Component
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        Created with{' '}
+        <i className="fa fa-heart" style={{ color: '#FF5252' }}></i> by
+        <a
+          href="https://www.linkedin.com/in/mansimar-anand/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="link"
+        >
+          <span> </span>
+          Mansimar Anand
+        </a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
