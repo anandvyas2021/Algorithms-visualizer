6fb0304966872ff60d1bd4288717565cde6c4e47
anand vyas <anandvyas1290@gmail.com>
2022-08-13 03:55:37 +0530

    web title and logo change

--- src/Component/footer.jsx
+++ src/Component/footer.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from "react";
 
 // Footer Component
 const Footer = () => {
   return (
     <footer>
       <p>
-        Created with{' '}
-        <i className="fa fa-heart" style={{ color: '#FF5252' }}></i> by
+        Created with{" "}
+        <i className="fa fa-heart" style={{ color: "#FF5252" }}></i> by
         <a
-          href="https://www.linkedin.com/in/mansimar-anand/"
+          href="https://www.linkedin.com/in/anandvyas1290/"
           target="_blank"
           rel="noopener noreferrer"
           className="link"
         >
           <span> </span>
-          Mansimar Anand
+          Anand Vyas
         </a>
       </p>
     </footer>
